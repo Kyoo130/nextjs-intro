3d998f786fa9b7b19ca7993bf9de0f8558faa4bc
Kyoo130 <kml9003@kakao.com>
2022-07-14 01:29:03 +0900

    :lipstick: Nav 스타일 적용 (module css 사용)

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,14 +1,22 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import styles from "./NavBar.module.css";
 
 export default function NavBar() {
+  const router = useRouter();
   return (
     <nav>
       <Link href="/">
-        <a>Home</a>
+        <a className={`${styles.link} ${router.pathname === "/" ? styles.active : ""}`}>Home</a>
       </Link>
       <Link href="/about">
-        <a>About</a>
+        <a className={[
+          styles.link,
+          router.pathname === "/about" ? styles.active : ""
+        ].join(" ")}>
+          About
+        </a>
       </Link>
     </nav>
-  )
+  );
 }
